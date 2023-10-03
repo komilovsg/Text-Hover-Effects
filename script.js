57e0446d5e92d0fe9c7a29d5d50b62976b51b715
komilovsg <komilovsg@gmail.com>
2023-10-03 15:14:35 +0500

    text-hover

---
+++ script.js
@@ -0,0 +1,10 @@
+// window.addEventListener('scroll', function() {
+//     var scrollPosition = window.scrollY;
+//     var boxHeight = document.querySelector('.box').offsetHeight;
+//     var h1 = document.querySelectorAll('.box h1');
+    
+//     h1[1].style.clipPath = 'polygon(0 0, 100% 0, 100% ' + (scrollPosition / boxHeight * 100) + '%, 0% ' + (scrollPosition / boxHeight * 100) + '%)';
+//     h1[0].style.clipPath = 'polygon(0 0, 100% 0, 100% ' + (scrollPosition / boxHeight * 100) + '%, 0% ' + (scrollPosition / boxHeight * 100) + '%)';
+//   });
+
+// Тут должна была быть продолжение логической части работы...  
